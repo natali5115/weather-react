710181c77d4a85f53b0017e8cfa6f7aca3280066
natali5115 <46550776+natali5115@users.noreply.github.com>
2022-03-21 22:29:44 -0400

    edits

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
 
 function formatDate(date) {
   let hours = date.getHours();
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Axios from 'axios';
+
 
 import logo from './logo.svg';
 import './App.css';
