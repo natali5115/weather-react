ef1aba3f4044e44ec2cb4a80480c6a6efd5e56d4
natali5115 <46550776+natali5115@users.noreply.github.com>
2022-03-21 22:41:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
 
 class Timer extends React.Component {
   constructor(props) {
@@ -60,3 +38,31 @@ ReactDOM.render(
   <Timer />,
   document.getElementById('timer-example')
 );
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
